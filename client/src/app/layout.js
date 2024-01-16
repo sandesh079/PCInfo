dae67c9b5607f25ad157e2bccc5cedfd09445efc
sandesh079 <sandesh.dahal022@gmail.com>
2024-01-16 17:47:21 +0545

    login and resignter page

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import {Providers} from "./providers";
+import NavbarComponent from '@/components/NavbarComponent';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,11 +13,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className=''>
       
       <body className={inter.className}>
         <Providers>
+        <NavbarComponent/>
         {children}
+        <Footer/>
         </Providers>
       </body>
     </html>
