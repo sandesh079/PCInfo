4ab809013c105233f9814a0f227ded00c91a38f9
sandesh079 <sandesh.dahal022@gmail.com>
2024-02-16 12:53:23 +0545

    edit form components created

--- client/src/components/admin/productTable.js
+++ client/src/components/admin/productTable.js
@@ -4,11 +4,15 @@ import { Table, Popconfirm, Button, Modal, Form } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import { Input, Upload, Select } from "antd";
 
-
-
-const ProductTable = ({ products, categories, onDelete,handleEdit }) => {
+const ProductTable = ({
+  products,
+  categories,
+  onDelete,
+  handleEdit,
+  addLoading,
+}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [product,setProduct] = useState({})
+  const [product, setProduct] = useState({});
 
   const handleOk = () => {
     setIsModalOpen(false);
@@ -22,7 +26,6 @@ const ProductTable = ({ products, categories, onDelete,handleEdit }) => {
     form.setFieldsValue(record);
     setIsModalOpen(true);
   };
-  
 
   const columns = [
     {
@@ -103,7 +106,9 @@ const ProductTable = ({ products, categories, onDelete,handleEdit }) => {
         <>
           <div className="grid grid-cols-2">
             <div className="col-span-1">
-            <Button type="link" onClick={() => showModal(record)}>Edit</Button>
+              <Button type="link" onClick={() => showModal(record)}>
+                Edit
+              </Button>
             </div>
             <div className="col-span-1">
               <Popconfirm
@@ -132,143 +137,159 @@ const ProductTable = ({ products, categories, onDelete,handleEdit }) => {
         scroll={{ x: 1500 }}
         pagination={false}
       />
-
-<Modal 
-      footer={false}
-      open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        {
-          product && 
+      {JSON.stringify(product)}
+      <Modal
+        footer={false}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {product && (
           <Form
-          form={form}
-          key={product._id}
-          layout="vertical"
-          className="max-w-full  "
-        >
-          <h2 className="text-3xl p-4 flex justify-center font-semibold leading-7 text-gray-900">
-            Add New Product
-          </h2>
-          <div className="grid grid-cols-2 gap-4">
-            <Form.Item
+            form={form}
+            key={product._id}
+            layout="vertical"
+            className="max-w-full  "
+            onFinish={(value) => {
+              handleEdit(value);
+              handleCancel()
+            }}
+          >
+            <h2 className="text-3xl p-4 flex justify-center font-semibold leading-7 text-gray-900">
+              Add New Product
+            </h2>
+            <div className="grid grid-cols-2 gap-4">
+              <Form.Item
+                label="Product Name"
+                className=""
+                style={{
+                  display: "none",
+                }}
+                name="_id"
+                rules={[
+                  { required: true, message: "Please input product name!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-              label="Product Name"
-              className=""
-              name="productName"
-              rules={[{ required: true, message: "Please input product name!" }]}
-            >
-              <Input defaultValue={product.productName} />
-            </Form.Item>
-    
-            <Form.Item
-              label="Processor"
-              name="processor"
-              rules={[{ required: true, message: "Please input processor!" }]}
-            >
-              <Input />
-            </Form.Item>
-    
-            <Form.Item
-              label="Brand"
-              name="brand"
-              rules={[{ required: true, message: "Please select brand!" }]}
-            >
-              <Select>
-                {categories.map((category) => (
-                  <Option key={category._id} value={category.category}>
-                    {category.category}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-    
-            <Form.Item
-              label="Category"
-              name="category"
-              rules={[{ required: true, message: "Please select category!" }]}
-            >
-              <Select>
-                {categories.map((category) => (
-                  <Option key={category._id} value={category.subCategory}>
-                    {category.subCategory}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-    
-            <Form.Item
-              label="Storage"
-              name="storage"
-              rules={[{ required: true, message: "Please input storage!" }]}
-            >
-              <Input />
-            </Form.Item>
-    
-            <Form.Item
-              label="RAM"
-              name="ram"
-              rules={[{ required: true, message: "Please input RAM!" }]}
-            >
-              <Input />
-            </Form.Item>
-    
-            <Form.Item
-              label="Available in Stock"
-              name="stock"
-              rules={[{ required: true, message: "Please input stock!" }]}
-            >
-              <Input />
-            </Form.Item>
-    
-            <Form.Item
-              label="Price"
-              name="price"
-              rules={[{ required: true, message: "Please input price!" }]}
-            >
-              <Input />
-            </Form.Item>
-    
-            <Form.Item
-              label="Product description"
-              name="description"
-              className="col-span-2"
-              rules={[{ required: true, message: "Please input description!" }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-    
-            <Form.Item
-              label="Image"
-              className="col-span-2"
-              name="image"
-              rules={[{ required: true, message: "Please upload image!" }]}
-            >
-              <Upload>
-                <Button icon={<UploadOutlined />}>Upload</Button>
-              </Upload>
-            </Form.Item>
-          </div>
-    
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <Button
-              htmlType="button"
-              onClick={handleCancel}
-              danger
-              className="text-base font-semibold leading-6 text-gray-900"
-            >
-              Cancel
-            </Button>
-            <Button
-              // loading={addLoading}
-              type="primary"
-              htmlType="submit"
-              className="rounded-md bg-indigo-600 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Add Product
-            </Button>
-          </div>
-        </Form>
-        }
- 
-       
+              <Form.Item
+                label="Product Name"
+                className=""
+                name="productName"
+                rules={[
+                  { required: true, message: "Please input product name!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Processor"
+                name="processor"
+                rules={[{ required: true, message: "Please input processor!" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Brand"
+                name="brand"
+                rules={[{ required: true, message: "Please select brand!" }]}
+              >
+                <Select>
+                  {categories.map((category) => (
+                    <Option key={category._id} value={category.category}>
+                      {category.category}
+                    </Option>
+                  ))}
+                </Select>
+              </Form.Item>
+
+              <Form.Item
+                label="Category"
+                name="category"
+                rules={[{ required: true, message: "Please select category!" }]}
+              >
+                <Select>
+                  {categories.map((category) => (
+                    <Option key={category._id} value={category.subCategory}>
+                      {category.subCategory}
+                    </Option>
+                  ))}
+                </Select>
+              </Form.Item>
+
+              <Form.Item
+                label="Storage"
+                name="storage"
+                rules={[{ required: true, message: "Please input storage!" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="RAM"
+                name="ram"
+                rules={[{ required: true, message: "Please input RAM!" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Available in Stock"
+                name="stock"
+                rules={[{ required: true, message: "Please input stock!" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Price"
+                name="price"
+                rules={[{ required: true, message: "Please input price!" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Product description"
+                name="description"
+                className="col-span-2"
+                rules={[
+                  { required: true, message: "Please input description!" },
+                ]}
+              >
+                <Input.TextArea />
+              </Form.Item>
+
+              <Form.Item label="Image" className="col-span-2" name="image">
+                <Upload>
+                  <Button icon={<UploadOutlined />}>Upload</Button>
+                </Upload>
+              </Form.Item>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <Button
+                htmlType="button"
+                onClick={handleCancel}
+                danger
+                className="text-base font-semibold leading-6 text-gray-900"
+              >
+                Cancel
+              </Button>
+              <Button
+                loading={addLoading}
+                type="primary"
+                htmlType="submit"
+                className="rounded-md bg-indigo-600 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add Product
+              </Button>
+            </div>
+          </Form>
+        )}
       </Modal>
     </div>
   );
--- client/src/components/product-card/page.js
+++ client/src/components/product-card/page.js
@@ -6,30 +6,64 @@ import Link from "next/link";
 const { Meta } = Card;
 const ProductCard = (props) => {
   return (
-    <div className="inline-flex">
-      <Link href={"/products/"+props?.item?._id}>
-      <Card
-        hoverable
-        style={{
-          width: 250,
-          margin: 8,
-        }}
-        cover={
-          <img
-            alt="Product"
-            src={props?.item?.image}
-          />
-        }
+    <Link
+      className="relative  flex  w-auto h-auto flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
+      href={"/products/" + props?.item?._id}
+    >
+      <a
+        className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+        href="#"
       >
-        
-        <Meta
-          title={props?.item?.productName}
-          description={props?.item?.description}
-        />
-        <Meta title={"Rs. " + props?.item?.price} />
-      </Card>
-      </Link>
-    </div>
+        <div className="h-auto w-[250px]">
+          <img
+            className="object-contain h-full w-full"
+            src={props?.item?.image}
+            alt="product image"
+          />
+        </div>
+
+        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+          {props?.item?.brand}
+        </span>
+      </a>
+      <div className="mt-4 px-5 pb-5">
+        <a href="#">
+          <h5 className="text-xl font-bold  text-slate-900">
+            {props?.item?.productName}
+          </h5>
+        </a>
+        <div className="w-[240px] h-auto">
+          <p>{props?.item?.processor}</p>
+        </div>
+
+        <div className="mt-2 mb-5 flex items-center justify-between">
+          <p>
+            <span className="text-xl font-bold text-blue-700">NPR. {props?.item?.price}</span>
+            <span className="text-sm  text-slate-900 line-through">$699</span>
+          </p>
+        </div>
+        <a
+          href="#"
+          className="flex items-center justify-center rounded-md bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+          Add to cart
+        </a>
+      </div>
+    </Link>
   );
 };
 
